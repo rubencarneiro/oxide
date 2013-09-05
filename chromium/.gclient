f54c93f466635ebf8481bb7ab96c6ceaa41e0b3c
Chris Coulson <chris.coulson@canonical.com>
2013-09-05 18:53:56 +0100

    Add a script for checking out depot_tools / chromium

--- chromium/.gclient
+++ chromium/.gclient
@@ -4,6 +4,11 @@ solutions = [
     "deps_file"   : "DEPS",
     "managed"     : True,
     "custom_deps" : {
+      "src/chrome/tools/test/reference_build/chrome_win": None,
+      "src/chrome_frame/tools/test/reference_build/chrome_win": None,
+      "src/chrome/tools/test/reference_build/chrome_linux": None,
+      "src/chrome/tools/test/reference_build/chrome_mac": None,
+      "src/third_party/hunspell_dictionaries": None
     },
     "safesync_url": "",
   },
---
+++ client.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# vim:expandtab:shiftwidth=2:tabstop=2:
+
+# Copyright (C) 2013 Canonical Ltd.
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+import os
+import os.path
+from subprocess import check_call, Popen, CalledProcessError
+
+DEPOT_TOOLS = "https://chromium.googlesource.com/chromium/tools/depot_tools.git"
+DEPOT_TOOLS_REV = "3b5efdf64d35ba20f8afd4f568eb190c3f1e8e12"
+
+topsrcdir = os.path.abspath(os.path.join(__file__, os.pardir))
+chromiumdir = os.path.join(topsrcdir, "chromium")
+
+def CheckCall(args, cwd = None):
+  if cwd is None:
+    check_call(args)
+  else:
+    r = Popen(args, cwd=cwd).wait()
+    if r is not 0: raise CalledProcessError(r, args)
+
+def main():
+  depot_tools_path = os.path.join(chromiumdir, "depot_tools")
+  if not os.path.isdir(depot_tools_path):
+    check_call(["git", "clone", DEPOT_TOOLS, depot_tools_path])
+
+  CheckCall(["git", "config", "remote.origin.url", DEPOT_TOOLS], depot_tools_path)
+  CheckCall(["git", "pull", "origin", "master"], depot_tools_path)
+  CheckCall(["git", "checkout", DEPOT_TOOLS_REV], depot_tools_path)
+  os.environ["PATH"] = os.path.join(chromiumdir, "depot_tools") + ":" + os.getenv("PATH")
+
+  chromium_src_path = os.path.join(chromiumdir, "src")
+  has_hg = os.path.isdir(os.path.join(chromium_src_path, ".hg"))
+  if has_hg:
+    CheckCall(["hg", "qpop", "-a"], chromium_src_path)
+
+  CheckCall(["gclient", "sync", "--force"], chromiumdir)
+
+  if not has_hg:
+    CheckCall(["hg", "init"], chromium_src_path)
+    CheckCall(["hg", "addremove"], chromium_src_path)
+    CheckCall(["hg", "ci", "-m", "Updated with client.py"], chromium_src_path)
+    CheckCall(["hg", "qinit"], chromium_src_path)
+
+    patchdir = os.path.join(topsrcdir, "patches")
+    with open(os.path.join(patchdir, "series"), "r") as f:
+      for line in f.readlines():
+        CheckCall(["hg", "qimport", os.path.join(patchdir, line.strip())], chromium_src_path)
+  else:
+    CheckCall(["hg", "addremove"], chromium_src_path)
+    CheckCall(["hg", "ci", "-m", "Updated with client.py"], chromium_src_path)
+
+  CheckCall(["hg", "qpush", "-a"], chromium_src_path)
+
+if __name__ == "__main__":
+  main()
