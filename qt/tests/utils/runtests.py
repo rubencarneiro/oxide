9f866173fe10c86b2251fdaf1f543e7e9aaec719
Chris Coulson <chris.coulson@canonical.com>
2013-09-27 16:30:08 +0100

    Add support for creating dynamic responses from the HTTP server by running python

--- qt/tests/utils/runtests.py
+++ qt/tests/utils/runtests.py
@@ -18,6 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 import BaseHTTPServer
+from cStringIO import StringIO
 from optparse import OptionParser
 import os
 import os.path
@@ -29,7 +30,89 @@ from subprocess import Popen
 import sys
 import tempfile
 import thread
+import traceback
 import urllib
+
+class PythonHandlerSandboxGlobal(dict):
+  def __init__(self):
+    dict.__init__(self, {
+      '__builtins__': __builtins__
+    })
+
+  def __setitem__(self, key, value):
+    raise TypeError("PythonHandlerSandboxGlobal does not support assignment")
+
+class PythonHandlerSandbox(object):
+  def __init__(self, f):
+    self._locals = dict()
+
+    _globals = PythonHandlerSandboxGlobal()
+
+    execfile(f, _globals, self._locals)
+
+  def run_handler(self, request):
+    self._locals["handler"](request)
+
+class PythonHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+  def __init__(self, request, translated_path):
+    self.command = request.command
+    self.path = request.path
+    self.request_version = request.request_version
+    self.headers = request.headers
+    self.rfile = request.rfile
+    self.requestline = request.requestline
+
+    self._request = request
+    self._translated_path = translated_path
+
+    BaseHTTPServer.BaseHTTPRequestHandler.__init__(
+        self, None, request.client_address, request.server)
+
+  def setup(self):
+    self.wfile = StringIO()
+
+  def handle(self):
+    try:
+      sandbox = PythonHandlerSandbox(self._translated_path)
+    except OSError:
+      self._request.send_error(404)
+      return
+    except:
+      self._request.send_error(500)
+      traceback.print_exc()
+      return
+
+    del self._translated_path
+
+    request = self._request
+    del self._request
+    
+    try:
+      sandbox.run_handler(self)
+    except:
+      request.send_error(500)
+      traceback.print_exc()
+
+    self._request = request
+    self._success = True
+
+  def finish(self):
+    try:
+      self.finish_internal()
+    except:
+      traceback.print_exc()
+
+  def finish_internal(self):
+    if not hasattr(self, "_success") or not self._success:
+      return
+
+    if self.wfile.closed:
+      self._request.send_error(500, "Python hook closed the write buffer")
+      return
+
+    self.wfile.flush()
+    self._request.wfile.write(self.wfile.getvalue())
+    self.wfile.close()
 
 class TestProcess(object):
   def __init__(self, runner, args):
@@ -55,7 +138,49 @@ class TestProcess(object):
     return self._rfd
 
 class TestHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+  def is_python_request(self):
+    (root, ext) = os.path.splitext(self.path)
+    if ext == ".py":
+      return True
+
+    return False
+
+  def execute_python(self):
+    path = self.translate_path(self.path)
+    if not os.path.isfile(path):
+      return False
+
+    PythonHTTPRequestHandler(self, path)
+
+    return True
+
+  def do_GET(self):
+    """Serve a GET request."""
+    if self.is_python_request() and self.execute_python():
+      return
+
+    f = self.send_head()
+    if f:
+      self.copyfile(f, self.wfile)
+      f.close()
+
+  def do_HEAD(self):
+    """Serve a HEAD request."""
+    if self.is_python_request() and self.execute_python():
+      return
+
+    f = self.send_head()
+    if f:
+      f.close()
+
   def translate_path(self, path):
+    """Translate a /-separated PATH to the local filename syntax.
+
+    Components that mean special things to the local file system
+    (e.g. drive or directory names) are ignored.  (XXX They should
+    probably be diagnosed.)
+
+    """
     # abandon query parameters
     path = path.split('?',1)[0]
     path = path.split('#',1)[0]
