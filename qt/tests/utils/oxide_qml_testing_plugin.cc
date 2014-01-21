fce53e57460220b5888f070822bd615181027901
Chris Coulson <chris.coulson@canonical.com>
2014-01-21 21:44:24 +0000

    Add a test for WebView.preferences

--- qt/tests/utils/oxide_qml_testing_plugin.cc
+++ qt/tests/utils/oxide_qml_testing_plugin.cc
@@ -18,32 +18,107 @@
 #include <QDir>
 #include <QLatin1String>
 #include <QQmlContext>
-#include <QQmlEngine>
 #include <QQmlExtensionPlugin>
 #include <QString>
 #include <QtGlobal>
+#include <QtQml>
 #include <QUrl>
-#include <QVariant>
 
-class OxideQmlTestingPlugin : public QQmlExtensionPlugin {
-  Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface" FILE "oxide_qml_testing_plugin.json")
+class DestructionObserver : public QObject {
   Q_OBJECT
- public:
-  void initializeEngine(QQmlEngine* engine, const char* uri) {
-    Q_ASSERT(QLatin1String(uri) == QLatin1String("com.canonical.Oxide.Testing"));
+  Q_PROPERTY(bool destroyed READ destroyed NOTIFY destroyedChanged)
 
-    QUrl url;
+ public:
+  DestructionObserver(QObject* object) :
+      destroyed_(false) {
+    connect(object, SIGNAL(destroyed()),
+            this, SLOT(onDestroyed()));
+  }
+
+  virtual ~DestructionObserver() {
+    disconnect(this, SLOT(onDestroyed()));
+  }
+
+  bool destroyed() const { return destroyed_; }
+
+ Q_SIGNALS:
+  void destroyedChanged();
+
+ private Q_SLOTS:
+  void onDestroyed() {
+    Q_ASSERT(!destroyed_);
+
+    destroyed_ = true;
+    Q_EMIT destroyedChanged();
+  }
+
+ private:
+  bool destroyed_;
+};
+
+class OxideTestingUtils : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(QUrl DATA_PATH READ dataPath CONSTANT)
+
+ public:
+  OxideTestingUtils() {}
+
+  QUrl dataPath() const {
+    static QUrl url;
+    static bool initialized = false;
+
+    if (initialized) {
+      return url;
+    }
+
+    initialized = true;
+
     QString path(QString(qgetenv("OXIDE_TESTING_DATA_PATH")));
     if (!path.isEmpty()) {
       QDir dir(path);
       url = QUrl::fromLocalFile(dir.absolutePath());
     }
-    engine->rootContext()->setContextProperty("OXIDE_TESTING_DATA_PATH",
-                                              QVariant(url));
+
+    return url;
   }
 
+  Q_INVOKABLE QObject* qObjectParent(QObject* object) {
+    if (!object) {
+      return NULL;
+    }
+
+    return object->parent();
+  }
+
+  Q_INVOKABLE DestructionObserver* createDestructionObserver(QObject* object) {
+    if (!object) {
+      return NULL;
+    }
+
+    return new DestructionObserver(object);
+  }
+};
+
+QObject* UtilsFactory(QQmlEngine* engine, QJSEngine* script_engine) {
+  Q_UNUSED(engine);
+  Q_UNUSED(script_engine);
+
+  return new OxideTestingUtils();
+}
+
+class OxideQmlTestingPlugin : public QQmlExtensionPlugin {
+  Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface" FILE "oxide_qml_testing_plugin.json")
+  Q_OBJECT
+
+ public:
   void registerTypes(const char* uri) {
-    Q_UNUSED(uri);
+    Q_ASSERT(QLatin1String(uri) == QLatin1String("com.canonical.Oxide.Testing"));
+
+    qmlRegisterSingletonType<OxideTestingUtils>(
+        uri, 0, 1, "OxideTestingUtils", UtilsFactory);
+    qmlRegisterUncreatableType<DestructionObserver>(
+        uri, 0, 1, "DestructionObserver",
+        "Create this with OxideTestingUtils.createDestructionObserver()");
   }
 };
 
