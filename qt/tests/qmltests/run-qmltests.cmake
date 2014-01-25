e4f060881c0d938223e25791536ce2786bbd0123
Chris Coulson <chris.coulson@canonical.com>
2014-01-25 12:07:18 +0000

    Add license headers

--- qt/tests/qmltests/run-qmltests.cmake
+++ qt/tests/qmltests/run-qmltests.cmake
@@ -1,3 +1,21 @@
+# vim:expandtab:shiftwidth=2:tabstop=2:
+
+# Copyright (C) 2014 Canonical Ltd.
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 foreach(v CHROMIUM_PRODUCT_DIR CHROMIUM_LIB_DIR SOURCE_DIR SERVER_DIR BINARY_DIR TEST_DIR TEST_NAME)
   if(NOT DEFINED ${v})
     message(FATAL_ERROR "Must specify ${v}")
