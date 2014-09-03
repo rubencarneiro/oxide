1ba5a864d38cee04f7e91e30f9959338756e1584
Chris Coulson <chris.coulson@canonical.com>
2014-09-04 00:39:31 +0100

    Add @run-at document-start to TestUtilsSlave.js to hopefully fix some random test failures that look like a race

--- qt/tests/qmltests/TestUtilsSlave.js
+++ qt/tests/qmltests/TestUtilsSlave.js
@@ -15,6 +15,10 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+// ==UserScript==
+// @run-at document-start
+// ==/UserScript==
+
 oxide.addMessageHandler("GET-DOCUMENT-URI", function(msg) {
   msg.reply({location: window.document.documentURI});
 });
