9d2b8d08c396dd3845eaee45bbabc1b5ec152413
Michael Terry <michael.terry@canonical.com>
2015-04-03 14:08:21 -0400

    Drop change to context-dependent.cc file

--- qt/core/gpu/oxide_qt_gl_context_dependent.cc
+++ qt/core/gpu/oxide_qt_gl_context_dependent.cc
@@ -65,7 +65,7 @@ scoped_refptr<GLContextDependent> GLContextDependent::Create(
           new GLContextDependent(handle,
                                  gfx::kGLImplementationEGLGLES2));
     }
-  } else if (platform.startsWith("ubuntu") || platform == "mirserver") {
+  } else if (platform.startsWith("ubuntu")) {
     void* handle = pni->nativeResourceForContext("eglcontext", context);
     if (handle) {
       return make_scoped_refptr(
