ee7e29a5a8199062fa9bdaa998c9da7b87873cdd
Chris Coulson <chris.coulson@canonical.com>
2015-08-04 21:44:09 +0100

    Bump Chromium rev to 46.0.2471.0

--- shared/browser/oxide_render_widget_host_view.cc
+++ shared/browser/oxide_render_widget_host_view.cc
@@ -303,11 +303,6 @@ void RenderWidgetHostView::SetIsLoading(bool is_loading) {
   UpdateCursorOnWebView();
 }
 
-void RenderWidgetHostView::TextInputTypeChanged(ui::TextInputType type,
-                                                ui::TextInputMode mode,
-                                                bool can_compose_inline,
-                                                int flags) {}
-
 void RenderWidgetHostView::ImeCancelComposition() {
   if (!delegate_) {
     return;
