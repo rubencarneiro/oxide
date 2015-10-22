28f2ef9125043980563bc0202ff85152ff914778
Chris Coulson <chris.coulson@canonical.com>
2015-10-23 00:20:57 +0100

    Work around lack of camera device enumeration on the phone

--- qt/core/browser/media/oxide_qt_video_capture_device_factory.cc
+++ qt/core/browser/media/oxide_qt_video_capture_device_factory.cc
@@ -17,8 +17,17 @@
 
 #include "oxide_qt_video_capture_device_factory.h"
 
+#include <algorithm>
+#include <map>
+#include <string>
+
+#include <QByteArray>
 #include <QCamera>
+#if QT_VERSION >= QT_VERSION_CHECK(5, 3, 0)
+#include <QCameraInfo>
+#endif
 #include <QList>
+#include <QString>
 
 #include "shared/browser/oxide_browser_platform_integration.h"
 
@@ -27,9 +36,54 @@
 namespace oxide {
 namespace qt {
 
+namespace {
+
+typedef std::map<media::VideoCaptureDevice::Name, QCamera::Position>
+    DefaultVideoDeviceNameMap;
+
+DefaultVideoDeviceNameMap GetDefaultDeviceNames() {
+  DefaultVideoDeviceNameMap rv;
+#if QT_VERSION >= QT_VERSION_CHECK(5, 3, 0)
+  const QCamera::Position positions[] = { QCamera::BackFace,
+                                          QCamera::FrontFace };
+
+  for (size_t i = 0; i < arraysize(positions); ++i) {
+    QCamera camera(positions[i]);
+    QCameraInfo info(camera);
+    if (info.deviceName().isEmpty()) {
+      // If there's no camera in the specified position, Qt will fall back to
+      // the default. If there's no default, it's game over
+      return rv;
+    }
+    if (!camera.isCaptureModeSupported(QCamera::CaptureVideo)) {
+      continue;
+    }
+
+    media::VideoCaptureDevice::Name device_name(
+        info.description().toStdString(),
+        info.deviceName().toStdString(),
+        media::VideoCaptureDevice::Name::API_TYPE_UNKNOWN);
+
+    rv[device_name] = info.position();
+  }
+#endif
+  return rv;
+}
+
+}
+
 scoped_ptr<media::VideoCaptureDevice> VideoCaptureDeviceFactory::Create(
     const media::VideoCaptureDevice::Name& device_name) {
   DCHECK(thread_checker_.CalledOnValidThread());
+
+  DefaultVideoDeviceNameMap default_names = GetDefaultDeviceNames();
+  if (default_names.find(device_name) != default_names.end()) {
+    // If the device name corresponds to a camera that is the default for its
+    // position, then we request the camera by position. This is to workaround
+    // a limitation in the QtUbuntu backend, which doesn't let us select a
+    // camera by its ID
+    return make_scoped_ptr(new VideoCaptureDevice(default_names[device_name]));
+  }
 
   return make_scoped_ptr(new VideoCaptureDevice(device_name));
 }
@@ -39,16 +93,46 @@ void VideoCaptureDeviceFactory::GetDeviceNames(
   DCHECK(thread_checker_.CalledOnValidThread());
   DCHECK(device_names->empty());
 
+#if QT_VERSION >= QT_VERSION_CHECK(5, 3, 0)
+  QList<QCameraInfo> devices = QCameraInfo::availableCameras();
+#else
   QList<QByteArray> devices = QCamera::availableDevices();
-  for (const QByteArray& camera_id: devices) {
-    QCamera camera(camera_id);
+#endif
+  for (const auto& info : devices) {
+    QCamera camera(info);
     if (!camera.isCaptureModeSupported(QCamera::CaptureVideo)) {
       continue;
     }
     device_names->push_back(media::VideoCaptureDevice::Name(
-        camera_id.data(),
-        camera_id.data(),
+#if QT_VERSION >= QT_VERSION_CHECK(5, 3, 0)
+        info.description().toStdString(),
+        info.deviceName().toStdString(),
+#else
+        info.data(),
+        info.data(),
+#endif
         media::VideoCaptureDevice::Name::API_TYPE_UNKNOWN));
+  }
+
+  // The QtUbuntu backend doesn't support device enumeration yet, so we
+  // emulate enumeration here in a second pass by constructing default
+  // back and forward facing cameras to try to establish if they exist
+
+  DefaultVideoDeviceNameMap default_names = GetDefaultDeviceNames();
+  for (const auto& name : default_names) {
+    const std::string& id = name.first.id();
+    bool duplicate = std::find_if(
+        device_names->begin(),
+        device_names->end(),
+        [id](const media::VideoCaptureDevice::Name& name) {
+          return name.id() == id;
+        }) != device_names->end();
+    if (duplicate) {
+      // Don't duplicate devices
+      continue;
+    }
+
+    device_names->push_back(name.first);
   }
 }
 
