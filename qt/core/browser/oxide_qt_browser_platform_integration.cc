4f2dc87188f3b239905ffcc1bfe73fca5721c21d
Olivier Tilloy <olivier.tilloy@canonical.com>
2016-01-08 21:59:44 +0100

    Update copyright years in headers of changed files.

--- qt/core/browser/oxide_qt_browser_platform_integration.cc
+++ qt/core/browser/oxide_qt_browser_platform_integration.cc
@@ -1,5 +1,5 @@
 // vim:expandtab:shiftwidth=2:tabstop=2:
-// Copyright (C) 2014-2015 Canonical Ltd.
+// Copyright (C) 2014-2016 Canonical Ltd.
 
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
